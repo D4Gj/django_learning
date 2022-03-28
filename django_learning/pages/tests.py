8d612f8e6fc54e478aeead3aeb75c22ae8294515
D4Gj <inzadimonax@gmail.com>
2022-03-28 19:12:45 +0400

    Message board

--- django_learning/pages/tests.py
+++ django_learning/pages/tests.py
@@ -1,10 +1,12 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
+from .models import Post
 
-class SimpleTests(SimpleTestCase):
-    def test_home_page_status(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
 
-    def test_about_page_status(self):
-        response = self.client.get("/about/")
-        self.assertEqual(response.status_code, 200)
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text="test")
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_post_name = f"{post.text}"
+        self.assertEqual(expected_post_name, "test")
