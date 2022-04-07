5dba32c5cfbe1efe324aa359d7cff259880573d6
D4Gj <inzadimonax@gmail.com>
2022-04-07 19:29:17 +0400

    detail_view

--- django_learning/pages/tests.py
+++ django_learning/pages/tests.py
@@ -1,12 +1,31 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 from .models import Post
 
 
 class PostModelTest(TestCase):
     def setUp(self):
-        Post.objects.create(text="test")
+        self.user = get_user_model().objects.create_user(
+            username='test',
+            email='test@email.com',
+            password='secret'
+        )
 
-    def test_text_content(self):
-        post = Post.objects.get(id=1)
-        expected_post_name = f"{post.text}"
-        self.assertEqual(expected_post_name, "test")
+        self.post = Post.objects.create(
+            title='A',
+            body="ABC",
+            author=self.user,
+        )
+
+    def test_string_repr(self):
+        post = Post(title='A')
+        self.assertEqual(str(post), post.title)
+
+    def test_post_detail(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/1000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'A')
+        self.assertTemplateUsed(response, 'post_detail.html')
--- django_learning/pages/views.py
+++ django_learning/pages/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
@@ -7,3 +7,8 @@ class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'all_posts_list'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
